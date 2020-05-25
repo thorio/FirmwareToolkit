a20bf7086d0968ea3cf2af5231ffa01d02ba1bd2
thorio <thorio.git@gmail.com>
2020-05-25 20:12:23 +0200

    Add name selector

---
+++ Selectors/NameSelector.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IngameScript.Selectors
+{
+	class NameSelector<T> : TagSelector<T> where T : IMyTerminalBlock
+	{
+		public NameSelector(string name) : base(name) { }
+
+		public override IEnumerable<T> GetBlocks_Inner() =>
+			base.GetBlocks_Inner().Where((b) => b.Name == _identifier);
+	}
+}
